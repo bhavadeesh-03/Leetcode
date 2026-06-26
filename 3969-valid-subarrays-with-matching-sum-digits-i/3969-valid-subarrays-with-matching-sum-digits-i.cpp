0c33e1fb75891bdac563dea7da2e9dc40f3802af
Garikipati Bhavadeesh <g.bhavadeesh207@outlook.com>
2026-06-26 15:38:49 +0530

    Time: 1176 ms (5.02%), Space: 32.4 MB (48.17%) - LeetHub

--- 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.cpp
+++ 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int countValidSubarrays(vector<int>& a, int x) {
-        long long ans,c = 0;
-        string s;
-        for(int i = 0;i < a.size();i++) {
-            ans = 0;
-            for(int j = i;j < a.size();j++) {
+        int n = a.size();
+        long long c = 0;
+        for(int i = 0;i < n;i++) {
+            long long ans = 0;
+            for(int j = i;j < n;j++) {
                 ans += a[j];
                 int first = ans / pow(10, (int)log10(ans));
                 if(first == x && ans % 10 == x) c++;
